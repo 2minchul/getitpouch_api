003862b786411bb1cc03ea8a5a5e96ae105f7511
LMC <2_minchul@naver.com>
2017-05-14 08:11:30 +0900

    make it better

--- getitpouch_api.py
+++ getitpouch_api.py
@@ -1,11 +1,49 @@
 from flask import Flask
+import pymongo
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+
+connection = pymongo.MongoClient("mongodb://%s:%d" % (MONGODB_HOST, MONGODB_PORT))
+db = connection.db
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Running!'
+
+
+@app.route('rank', methods=['POST'])
+def rank():
+    return ''
+
+
+@app.route('scan', methods=['POST'])
+def scan():
+    return ''
+
+
+@app.route('search', methods=['POST'])
+def search():
+    return ''
+
+
+@app.route('my_pouch/list', methods=['POST'])
+def my_list():
+    return ''
+
+
+@app.route('my_pouch/add_item', methods=['POST'])
+def pouch_add():
+    return ''
+
+
+@app.route('my_pouch/delete', methods=['POST'])
+def pouch_del():
+    return ''
 
 
 if __name__ == '__main__':
